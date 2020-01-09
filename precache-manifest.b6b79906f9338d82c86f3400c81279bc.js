2e3fa3d62cebdba722dea7e9037fd36c71455bb9
assanmboge <mbogeassan77@gmail.com>
2020-01-09 05:29:40 +0200

    Updates

---
+++ precache-manifest.b6b79906f9338d82c86f3400c81279bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b1b6f824a146a8fce2175bc9a83846c",
+    "url": "/testd/index.html"
+  },
+  {
+    "revision": "fa0e18ddfdd3a1910be4",
+    "url": "/testd/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1f8ef11defcb3558664e",
+    "url": "/testd/static/js/2.9d2a95c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/testd/static/js/2.9d2a95c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fa0e18ddfdd3a1910be4",
+    "url": "/testd/static/js/main.742b4bc2.chunk.js"
+  },
+  {
+    "revision": "9c8f2cb8e64a99e5b35a",
+    "url": "/testd/static/js/runtime-main.dc019314.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testd/static/media/logo.5d5d9eef.svg"
+  }
+]);
